7a11bf7d76ac1b64d7a4bfde391b934ae8e37f8a
Syed Hussain <sshussai@ualberta.ca>
2019-01-01 10:47:48 -0700

    create the users app, and add it to settings.py file

--- r3member_project/r3member_project/settings.py
+++ r3member_project/r3member_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
 ]
 
 MIDDLEWARE = [
@@ -78,7 +79,7 @@ DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    
+
     #'default': {
     #    'ENGINE': 'django.db.backends.mysql',
     #    'NAME': 'r3member',
