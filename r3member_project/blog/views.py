9d9424d875568cd1a01c3794a2c7050d7ec0ea85
Syed Hussain <sshussai@ualberta.ca>
2019-01-10 22:03:30 -0700

    add class based listview to display posts in the homepage and update urls to point to the new view

--- r3member_project/blog/views.py
+++ r3member_project/blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
+
 
 # Create your views here.
 def home(request):
@@ -8,5 +10,25 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+
+# Class based view for the home page
+class PostListView(ListView):           # inherit from ListView
+    model = Post                        # Set the model to be queried for list
+    # change the name of the default template used by the ListView
+    template_name = 'blog/home.html'    # default template: <app>/<model><viewtype>.html
+    # set an attribute to rename the object to be presented in a list
+    context_object_name = 'posts'       # default var: object_list
+    # order by inverse date_posted
+    ordering = ['-date_posted']
+
+
 def about(request):
     return render(request, 'blog/about.html')
+
+
+'''
+Class based views - generic views for different, generic functionalities
+    -different types (update, list, detail views, etc)
+    -Use a template with the following naming pattern by default:
+        <app>/<model>_<viewtype>.html
+'''
