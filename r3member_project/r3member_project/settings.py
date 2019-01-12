5b5d449e931733def70462960dea6afcaefb20dd
Syed Hussain <sshussai@ualberta.ca>
2019-01-11 18:24:31 -0700

    add views to reset password using class views, and add necessary templates. also add email info on settings.py

--- r3member_project/r3member_project/settings.py
+++ r3member_project/r3member_project/settings.py
@@ -142,3 +142,11 @@ CRISPY_TEMPLATE_PACK='bootstrap4'
 LOGIN_REDIRECT_URL = 'blog-home'
 # Login page
 LOGIN_URL = 'login'
+
+# Email settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
