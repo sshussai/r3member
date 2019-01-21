044307e8c292f3fac95b7d898c68e0cb810fac95
Syed Hussain <sshussai@ualberta.ca>
2019-01-21 09:27:33 -0700

    add code textbox to model, and a bool field to determine if there is code or not

--- r3member_project/blog/models.py
+++ r3member_project/blog/models.py
@@ -3,12 +3,17 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
 
+
 # Create your models here.
 class Post(models.Model):
+
     title = models.CharField(max_length=100)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # New fields
+    has_code = models.BooleanField(default=False)
+    code = models.TextField(default="")
 
     def __str__(self):
         return self.title
